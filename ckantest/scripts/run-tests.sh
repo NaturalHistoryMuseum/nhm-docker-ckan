e694ada3b228d5a39eb4870d349fc23d0ae33887
Ginger Butcher <alycejenni@gmail.com>
2022-11-04 19:21:50 +0000

    feat: add test run script
    script runs tests with or without coverage (if no module name is specified) and optionally uploads results to coveralls (if -c flag is used).

---
+++ ckantest/scripts/run-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while getopts 'c' OPTION; do
+  case "$OPTION" in
+    c)
+        RUN_COVERALLS=1;;
+    ?)
+        RUN_COVERALLS=0;
+        exit 1;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+if [ -n "$1" ]; then
+    echo "Running pytest --cov=$1 --ckan-ini=test.ini tests"
+    pytest --cov="$1" --ckan-ini=test.ini tests
+else
+    echo "Running pytest --ckan-ini=test.ini tests"
+    pytest --ckan-ini=test.ini tests
+fi
+
+if [ "$RUN_COVERALLS" = 1 ]; then
+    coveralls;
+fi
