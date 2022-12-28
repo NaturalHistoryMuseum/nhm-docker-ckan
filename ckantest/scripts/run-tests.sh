1e5cd2eb2dac2634f605a210ce5ac89ebcfd51c4
Ginger Butcher <alycejenni@gmail.com>
2022-12-28 12:30:20 +0000

    fix: exit with pytest exit code

--- ckantest/scripts/run-tests.sh
+++ ckantest/scripts/run-tests.sh
@@ -14,11 +14,15 @@ shift "$(($OPTIND -1))"
 if [ -n "$1" ]; then
     echo "Running pytest --cov=$1 --ckan-ini=test.ini tests"
     pytest --cov="$1" --ckan-ini=test.ini tests
+    test_exit_code=$?
 else
     echo "Running pytest --ckan-ini=test.ini tests"
     pytest --ckan-ini=test.ini tests
+    test_exit_code=$?
 fi
 
 if [ "$RUN_COVERALLS" = 1 ]; then
     coveralls;
 fi
+
+exit $test_exit_code
