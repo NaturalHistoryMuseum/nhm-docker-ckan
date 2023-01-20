2bc199e51083f095a9b47f71c416d1e9b75d049a
Ginger Butcher <alycejenni@gmail.com>
2023-01-20 17:21:17 +0000

    feat: run set-permissions command
    has to run after the db has been set up, and this seemed like the easiest place to put it

--- ckantest/waits/all.sh
+++ ckantest/waits/all.sh
@@ -7,6 +7,11 @@ bash "${BASH_SOURCE%/*}/services/redis.sh"
 bash "${BASH_SOURCE%/*}/services/elasticsearch.sh"
 bash "${BASH_SOURCE%/*}/services/mongodb.sh"
 
+echo "Setting DB permissions..."
+export PGPASSWORD=${PGPASSWORD:-"password"}
+/venv/bin/ckan -c test.ini datastore set-permissions | psql -h db -U ckan
+echo "Permissions set"
+
 echo "All services up, running command"
 
 exec "$@"
--- ckantest/waits/basic.sh
+++ ckantest/waits/basic.sh
@@ -5,6 +5,11 @@ bash "${BASH_SOURCE%/*}/services/postgresql.sh"
 bash "${BASH_SOURCE%/*}/services/solr.sh"
 bash "${BASH_SOURCE%/*}/services/redis.sh"
 
+echo "Setting DB permissions..."
+export PGPASSWORD=${PGPASSWORD:-"password"}
+/venv/bin/ckan -c test.ini datastore set-permissions | psql -h db -U ckan
+echo "Permissions set"
+
 echo "All services up, running command"
 
 exec "$@"
